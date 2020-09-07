746e62fcb3b2b3369ad54e1799fbe3916f10470c
Mattis DALLEAU <mattisdalleau@gmail.com>
2020-09-07 13:15:27 +0400

    Create epi_dump.sh

---
+++ scripts/epi_dump.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/sh
+
+check_sudo_privileges() {
+        if [[ $EUID -ne 0 ]]; then
+                echo "This script must be runned as root:
+                exit 1
+        fi
+}
+
+check_fedora_version() {
+        cat /etc/fedora-release | grep "Fedora release 32"
+        if [[ $? -ne 0 ]]; then
+                echo "You nedd to run this script on Fedora 32"
+                exit 1
+        fi
+}
+
+check_git() {
+        if [ ! -f "/usr/bin/git" ]; then
+                sudo dnf install git
+                if [ ! -f "/usr/bin/git" ]; then
+                        echo "Could not install git"
+                        exit 1
+                fi
+        fi
+}
+
+check_repo() {
+        if [ ! -d "dump" ]; then
+                echo "Could not clone repository"
+                exit 1
+        fi
+}
+
+download_dump() {
+        cd /tmp
+        git clone https://github.com/epitech/dump dump
+        check_repo
+        cd dump
+        chmod +x install_packages_dump.sh
+        sudo sh install_packages_dump.sh
+}
+
+main() {
+        check_sudo_privileges
+        check_fedora_version
+        check_git
+        download_dump
+}
+
+main
